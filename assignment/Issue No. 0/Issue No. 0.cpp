87440300384e5af8c5f5406ab8239753aef87652
jiang-hr <jianghanrui@outlook.com>
2020-07-06 00:56:46 +0800

    first commit init

---
+++ assignment/Issue No. 0/Issue No. 0.cpp
@@ -0,0 +1,12 @@
+﻿// Issue No. 0.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 主程序为测试程序。将test各个data stucture是否工作正常。
+// 所有的 data stucture 均使用模板编写。看心情使用异常处理。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
